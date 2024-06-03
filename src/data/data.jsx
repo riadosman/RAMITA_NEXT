9992363cf91d97663f65e970ba54b2aff9afecf7
riadosman <riyados973@gmail.com>
2024-06-03 10:01:06 +0300

    edit abpout page

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -8,7 +8,12 @@ import {
   FaMagnifyingGlass,
   FaChartPie,
 } from "react-icons/fa6";
-
+import {
+  GiDiscussion,
+  GiAtom,
+  GiCheckMark,
+  GiAchievement,
+} from "react-icons/gi";
 export const ServiseCards = [
   {
     title: "تعرف على جمهورك المستهدف بشكل أفضل",
@@ -131,24 +136,24 @@ export const aboutData = [
     title: "الابتكار",
     description:
       "نحن نعمل باستمرار على تطوير الخدمات الإبداعية، وإيجاد طرق جديدة تلبي احتياجات عملائنا المتغيرة.",
-    img: <FaPeopleGroup />,
+    img: <GiAtom />,
   },
   {
     title: "المصداقية والشفافية",
     description:
       "نحن نهدف دائمًا إلى القيام بالشيء الصحيح من خلال الثقة والصدق مع عملائنا",
-    img: <FaPeopleGroup />,
+    img: <GiCheckMark />,
   },
   {
     title: "الكفاءة",
     description:
       "نعمل بكفاءة عالية لضمان تحقيق أفضل النتائج لعملائنا بأقل تكلفة",
-    img: <FaPeopleGroup />,
+    img: <GiAchievement />,
   },
   {
     title: "العمل الجماعي",
     description: "نؤمن بأهمية العمل الجماعي كعنصر أساسي للنجاح",
-    img: <FaPeopleGroup />,
+    img: <GiDiscussion />,
   },
 ];
 export const ourServicesData = [
